68dc47ed815f9d78139f20651139b766b0cbc4e8
Indudhar-GJ <gjindudhar@gmail.com>
2024-07-08 23:38:13 +0530

    Added references and diagrams

--- codes/Graph Algorithms for Fraud Detection Networks.cpp
+++ codes/Graph Algorithms for Fraud Detection Networks.cpp
@@ -1,33 +1,96 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
+#include <stack>
+
 using namespace std;
 
-void dfs(int node, unordered_map<int, vector<int>> &graph, vector<bool> &visited)
+class TransactionGraph
 {
-    visited[node] = true;
-    cout << "Visited node: " << node << endl;
-    for (int neighbor : graph[node])
+private:
+    unordered_map<int, vector<int>> adjacencyList;
+
+    void performDFS(int start, unordered_set<int> &visited, vector<int> &currentCluster)
     {
-        if (!visited[neighbor])
+        stack<int> toVisit;
+        toVisit.push(start);
+
+        while (!toVisit.empty())
         {
-            dfs(neighbor, graph, visited);
+            int currentNode = toVisit.top();
+            toVisit.pop();
+
+            if (visited.find(currentNode) == visited.end())
+            {
+                visited.insert(currentNode);
+                currentCluster.push_back(currentNode);
+
+                for (int neighbor : adjacencyList[currentNode])
+                {
+                    if (visited.find(neighbor) == visited.end())
+                    {
+                        toVisit.push(neighbor);
+                    }
+                }
+            }
         }
     }
-}
+
+public:
+    void recordTransaction(int sender, int receiver)
+    {
+        adjacencyList[sender].push_back(receiver);
+        adjacencyList[receiver].push_back(sender);
+    }
+
+    vector<vector<int>> findTransactionClusters()
+    {
+        unordered_set<int> visitedNodes;
+        vector<vector<int>> clusters;
+
+        for (const auto &entry : adjacencyList)
+        {
+            int account = entry.first;
+            if (visitedNodes.find(account) == visitedNodes.end())
+            {
+                vector<int> cluster;
+                performDFS(account, visitedNodes, cluster);
+                clusters.push_back(cluster);
+            }
+        }
+
+        return clusters;
+    }
+};
 
 int main()
 {
-    unordered_map<int, vector<int>> graph = {
-        {1, {2, 3}},
-        {2, {4}},
-        {3, {5}},
-        {4, {6}},
-        {5, {6}},
-        {6, {}}};
+    TransactionGraph transactionGraph;
+    int numberOfTransactions, sender, receiver;
 
-    vector<bool> visited(7, false);
-    dfs(1, graph, visited);
+    cout << "Enter the number of transactions: ";
+    cin >> numberOfTransactions;
+
+    cout << "Enter transactions (sender receiver):" << endl;
+    for (int i = 0; i < numberOfTransactions; ++i)
+    {
+        cin >> sender >> receiver;
+        transactionGraph.recordTransaction(sender, receiver);
+    }
+
+    vector<vector<int>> clusters = transactionGraph.findTransactionClusters();
+
+    cout << "Detected high volume transaction clusters:" << endl;
+    for (const auto &cluster : clusters)
+    {
+        cout << "Cluster: ";
+        for (int account : cluster)
+        {
+            cout << account << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
