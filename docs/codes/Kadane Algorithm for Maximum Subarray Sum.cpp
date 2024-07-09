8744f5121b8c9fe8facdab93e89f0b06b4a16f1f
Indudhar-GJ <gjindudhar@gmail.com>
2024-07-09 08:45:00 +0530

    corrected codes

--- docs/codes/Kadane Algorithm for Maximum Subarray Sum.cpp
+++ docs/codes/Kadane Algorithm for Maximum Subarray Sum.cpp
@@ -1,21 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int kadane(const vector<int> &arr)
+struct Result
 {
-    int maxSoFar = arr[0], maxEndingHere = arr[0];
-    for (size_t i = 1; i < arr.size(); ++i)
+    int maxSum;
+    int startIndex;
+    int endIndex;
+};
+
+Result maxSubarraySumWithLength(const vector<int> &arr, int length, bool findPositive)
+{
+    int maxSum = findPositive ? INT_MIN : INT_MAX;
+    int currentSum = 0;
+    int start = 0;
+    Result res = {findPositive ? INT_MIN : INT_MAX, -1, -1};
+
+    for (int i = 0; i < arr.size(); ++i)
     {
-        maxEndingHere = max(arr[i], maxEndingHere + arr[i]);
-        maxSoFar = max(maxSoFar, maxEndingHere);
+        currentSum += arr[i];
+        if ((findPositive && arr[i] > 0) || (!findPositive && arr[i] < 0))
+        {
+            if ((findPositive && currentSum > maxSum) || (!findPositive && currentSum < maxSum))
+            {
+                maxSum = currentSum;
+                res = {maxSum, start, i};
+            }
+        }
+        if (i >= length - 1)
+        {
+            currentSum -= arr[start];
+            ++start;
+        }
     }
-    return maxSoFar;
+    return res;
 }
 
 int main()
 {
     vector<int> arr = {1, -2, 3, 4, -1, 2, 1, -5, 4};
-    cout << "Maximum subarray sum is " << kadane(arr) << endl;
+    int length = 3;
+
+    Result maxPositive = maxSubarraySumWithLength(arr, length, true);
+    Result maxNegative = maxSubarraySumWithLength(arr, length, false);
+
+    if (maxPositive.maxSum != INT_MIN)
+    {
+        cout << "Maximum positive subarray sum with length " << length << " is "
+             << maxPositive.maxSum << " from index " << maxPositive.startIndex
+             << " to " << maxPositive.endIndex << endl;
+    }
+
+    if (maxNegative.maxSum != INT_MAX)
+    {
+        cout << "Maximum negative subarray sum with length " << length << " is "
+             << maxNegative.maxSum << " from index " << maxNegative.startIndex
+             << " to " << maxNegative.endIndex << endl;
+    }
+
     return 0;
 }
